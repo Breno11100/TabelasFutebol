a79e56b76031df8c38447f06729ed147b046ef11
Breno <breno.camilo.brc@gmail.com>
2026-05-14 13:03:59 -0300

    mudando direcionamentos

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function Brasileirao() {
   alert("Acessando tabela do Brasileirão");
-  window.location.href = "../html/brasileirao.html";
+  window.location.href = "../html/1.html";
 }
 
 function LaLiga() {
     alert("Acessando tabela da La Liga Espanhola")
-    window.location.href = "../html/laliga.html"
+    window.location.href = "../html/3.html"
 }
 
 function Bundesliga() {
     alert("Acessando tabela do Campeonato Alemão")
-    window.location.href = "../html/bundesliga.html"
+    window.location.href = "../html/2.html"
 }
 
 function PremierLeague() {
     alert("Acessando tabela do Campeonato Inglês")
-    window.location.href = "../html/premierleague.html"
+    window.location.href = "../html/4.html"
 }
